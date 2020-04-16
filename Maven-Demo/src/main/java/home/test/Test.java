e5125ffbee72d90216b2d4ac6c948fc983b992fb
sunilkhadanga <63045988+sunilkhadanga@users.noreply.github.com>
2020-04-16 16:05:56 +0530

    First Maven project commited

---
+++ Maven-Demo/src/main/java/home/test/Test.java
@@ -0,0 +1,12 @@
+package home.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Maven project testing................");
+
+	}
+
+}
